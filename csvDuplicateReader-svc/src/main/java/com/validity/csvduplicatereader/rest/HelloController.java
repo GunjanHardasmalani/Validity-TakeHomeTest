f6c0ff7c9f3f25041f08a7c8c1ada06e3cc42e3f
Gunjan Hardasmalani <hardasmalani.g@husky.neu.edu>
2020-07-14 23:25:17 -0400

    Create uploadCSV method in controller and test REST endpoint

--- csvDuplicateReader-svc/src/main/java/com/validity/csvduplicatereader/rest/HelloController.java
+++ csvDuplicateReader-svc/src/main/java/com/validity/csvduplicatereader/rest/HelloController.java
@@ -1,9 +1,18 @@
 package com.validity.csvduplicatereader.rest;
 
 import com.validity.csvduplicatereader.service.HelloService;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -17,5 +26,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String getHelloMessage() {
         return helloService.getHelloMessage();
+    }
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public @ResponseBody Map<String, String> uploadCSV(@RequestParam MultipartFile csvFile) {
+        Map<String, String> m = new HashMap<>();
+        m.put("test", "value");
+        return m;
     }
 }
