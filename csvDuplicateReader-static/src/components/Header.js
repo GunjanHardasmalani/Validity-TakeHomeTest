d97ed1704e9ca1cc6c9e9f462a189cd743745284
Gunjan Hardasmalani <hardasmalani.g@husky.neu.edu>
2020-07-14 22:25:00 -0400

    Create Header component

---
+++ csvDuplicateReader-static/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>CSV Duplicate Reader</h1>
+        </div>
+    );
+}
+
+export default Header;
